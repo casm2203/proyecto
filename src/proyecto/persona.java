82f76fcac933bdddec282421a75240dcb31844d9
SUAREZ MARTINEZ CRISTIAN <csuarez25@cuc.edu.co>
2019-10-28 20:16:31 -0500

    primera clase

---
+++ src/proyecto/persona.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+import java.util.Date;
+
+/**
+ *
+ * @author csuarez25
+ */
+public class persona {
+    private String nombre;
+    private String apellido;
+    private int id;
+    private Date nacimiento;
+    private String dirrecion;
+    private String email;
+    private String genero;
+    
+}
