5d913d37f75f158bf831e873be2628d1051480af
Cristian Suarez <cristian.suarez0322@gmail.com>
2019-10-31 13:52:53 -0500

    cargos arriba

--- src/proyecto/persona.java
+++ src/proyecto/persona.java
@@ -19,5 +19,71 @@ public class persona {
     private String dirrecion;
     private String email;
     private String genero;
+
+    public persona(String nombre, String apellido, int id, Date nacimiento, String dirrecion, String email, String genero) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.id = id;
+        this.nacimiento = nacimiento;
+        this.dirrecion = dirrecion;
+        this.email = email;
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getNacimiento() {
+        return nacimiento;
+    }
+
+    public void setNacimiento(Date nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    public String getDirrecion() {
+        return dirrecion;
+    }
+
+    public void setDirrecion(String dirrecion) {
+        this.dirrecion = dirrecion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
     
 }
